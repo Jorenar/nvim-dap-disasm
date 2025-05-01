1bdc05be2ce877e1997eb95cec74fbd9f8258276
Jorenar <dev@jorenar.com>
2025-05-01 11:27:42 +0200

    Add setup() function

--- lua/dap-disasm.lua
+++ lua/dap-disasm.lua
@@ -1,13 +1,14 @@
+local M = {}
+
 local dap = require("dap")
 
-vim.g.dap_disasm = vim.g.dap_disasm or {
-  beforeMemref = 16,
-  afterMemref  = 16,
+local disasm_bufnr = -1
+local req_defaults = {
+  address = "pc",
+  before = 16,
+  after  = 16,
 }
 
-vim.fn.sign_define("DapDisasmPC", {text="→", texthl="debugPC", linehl="debugPC"})
-
-local disasm_bufnr = -1
 local function get_disasm_bufnr()
   if not disasm_bufnr or not vim.api.nvim_buf_is_valid(disasm_bufnr) then
     disasm_bufnr = vim.api.nvim_create_buf(false, true)
@@ -44,9 +45,9 @@ local function write_buf(instructions, pc, jump_to_pc, cursor_offset)
   local buffer = get_disasm_bufnr()
   vim.api.nvim_buf_set_lines(buffer, 0, -1, false, lines)
 
-  vim.fn.sign_unplace("DisasmSigns", { buffer = buffer })
+  vim.fn.sign_unplace(M.config.sign, { buffer = buffer })
   if pc_line then
-    vim.fn.sign_place(0, "DisasmSigns", "DapDisasmPC", buffer, {
+    vim.fn.sign_place(0, "DisasmSigns", M.config.sign, buffer, {
         lnum = pc_line, priority = 10
       })
 
@@ -57,6 +58,36 @@ local function write_buf(instructions, pc, jump_to_pc, cursor_offset)
       vim.api.nvim_win_set_cursor(win, { pc_line, 0 })
     end
   end
+end
+
+local function request(session, pc, handler)
+  local memref = pc
+  if req_defaults.address ~= "pc" then
+    memref = req_defaults.address
+  end
+
+  local function get_ins_num(param, def)
+    local ret = def
+    if type(param) == "number" then
+      ret = param
+    elseif type(param) == "function" then
+      ret = param()
+    end
+    if (not ret) or (type(ret) ~= "number") or (ret < 0) then
+      return def
+    end
+    return ret
+  end
+
+  local ins_before = get_ins_num(M.config.ins_before_memref, req_defaults.before)
+  local ins_after = get_ins_num(M.config.ins_after_memref, req_defaults.after)
+
+  session:request("disassemble", {
+      memoryReference = memref,
+      instructionCount = ins_before + 1 + ins_after,
+      instructionOffset = -ins_before,
+      resolveSymbols = true,
+    }, handler)
 end
 
 local function render(jump_to_pc, cursor_offset)
@@ -76,18 +107,10 @@ local function render(jump_to_pc, cursor_offset)
     return
   end
 
-  local function handler(err, res)
+  request(session, pc, function(err, res)
     if err then return end
     write_buf(res.instructions or {}, pc, jump_to_pc, cursor_offset)
-  end
-
-  local g = vim.g.dap_disasm or {}
-  session:request("disassemble", {
-      memoryReference = g.memref or pc,
-      instructionCount = g.beforeMemref + 1 + g.afterMemref,
-      instructionOffset = -g.beforeMemref,
-      resolveSymbols = true,
-    }, handler)
+  end)
 end
 
 vim.api.nvim_create_autocmd("FileType" , {
@@ -106,14 +129,6 @@ vim.api.nvim_create_autocmd("FileType" , {
     end
   })
 
-if package.loaded["dapui"] then
-  require("dapui").register_element("disassembly", {
-      render = render,
-      buffer = get_disasm_bufnr,
-      allow_without_session = false,
-    })
-end
-
 vim.api.nvim_create_user_command("DapDisasm", function(t)
   vim.cmd(t.smods.vertical and "vsplit" or "split")
   local win = vim.api.nvim_get_current_win()
@@ -121,3 +136,30 @@ vim.api.nvim_create_user_command("DapDisasm", function(t)
   vim.api.nvim_win_set_buf(win, buf)
   render()
 end, {})
+
+M.config = {
+  dapui_register = true,
+  sign = "DapStopped",
+  ins_before_memref = req_defaults.before,
+  ins_after_memref = req_defaults.after,
+  -- columns = {
+  --   "address",
+  --   "instruction",
+  -- },
+}
+
+M.setup = function(conf)
+  M.config = vim.tbl_deep_extend("force", M.config, conf or {})
+
+  if M.config.dapui_register then
+    if package.loaded["dapui"] then
+      require("dapui").register_element("disassembly", {
+          render = render,
+          buffer = get_disasm_bufnr,
+          allow_without_session = false,
+        })
+    end
+  end
+end
+
+return M
